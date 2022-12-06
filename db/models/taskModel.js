481799e9b7d9608fc957bc32ded829a6ae3644ac
Nawfel Sekrafi <sekrafinawfel@gmail.com>
2022-12-06 16:18:21 +0100

    created task Schema

--- db/models/taskModel.js
+++ db/models/taskModel.js
@@ -1,32 +1,35 @@
 const mongoose = require("mongoose");
-const mongoosePaginate = require('mongoose-paginate-v2');
-const crypto = require("crypto");
-const bcrypt = require("bcryptjs");
+const mongoosePaginate = require("mongoose-paginate-v2");
 
 const taskSchema = new mongoose.Schema(
-    {
-        title: {
-            type: String,
-            trim: true
-        },
-        description: {
-            type: String,
-            trim: true
-        },
-        isCompleted : {
-            type: Boolean,
-            default: false
-        },
-        
-       //TODO comment
-       //TODO user
-        deletedAt: Date
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: [true, "Title can not be empty!"],
     },
+    description: {
+      type: String,
+      trim: true,
+      required: [true, "Description can not be empty!"],
+    },
+    status: {
+      enum: ["todo", "in progress", "completed"],
+      default: "todo",
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "task must has a user who did create it"],
+    },
+    sharedWith: [{ type: mongoose.Schema.ObjectId, ref: "User" }],
+    deletedAt: Date,
+  },
   {
     timestamps: true,
   }
 );
 
-taskSchema.plugin(mongoosePaginate)
+taskSchema.plugin(mongoosePaginate);
 
 module.exports = mongoose.model("Task", taskSchema);
