d3fc5f8bb25f33c9c18dd90bd4e99adea3cbda9a
Nawfel Sekrafi <sekrafinawfel@gmail.com>
2022-12-06 16:18:07 +0100

    created comment Schema

--- db/models/commentModel.js
+++ db/models/commentModel.js
@@ -1,21 +1,25 @@
 const mongoose = require("mongoose");
-const mongoosePaginate = require('mongoose-paginate-v2');
-const crypto = require("crypto");
-const bcrypt = require("bcryptjs");
 const commentSchema = new mongoose.Schema(
-    {
-        content: {
-            type: String, 
-            trim: true
-        },
-        //TODO user
-
+  {
+    content: {
+      type: String,
+      trim: true,
     },
-    {
-        timestamps: true,
-      }
-)
-
-commentSchema.plugin(mongoosePaginate)
+    // Parent Referencing
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Comment must has a user who did create it"],
+    },
+    task: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Task",
+      required: [true, "Comment must has a related task"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Comment", commentSchema);
